825071f0d1fd621ef67a63da5a0f3e81fdc65edf
priyavarshini-mediwave <priyavarshini@mindwaveventures.com>
2023-12-22 18:50:10 +0530

    EditUser

--- app/src/pages/EditUser.tsx
+++ app/src/pages/EditUser.tsx
@@ -1,4 +1,24 @@
+import { useEffect, useState } from "react";
+import { IaddUser } from "../Interfaces/interfaces";
+import { viewUserInfo } from "../services/api";
+
 const EditUser = () => {
+  const [editUser, setEditUser] = useState<IaddUser>();
+  console.log(editUser);
+  useEffect(() => {
+    async function toFindUserToEdit() {
+      try {
+        const userToEdit = await viewUserInfo();
+        if (userToEdit) {
+          console.log(userToEdit);
+          setEditUser(userToEdit.data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    toFindUserToEdit();
+  }, []);
   return (
     <>
       <h1>EditUser</h1>
